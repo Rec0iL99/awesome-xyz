6780988ef0df006f14f933c63f9b6c85e130aecc
Joel Mathew Koshy <joelmathewkoshy@gmail.com>
2023-11-08 01:49:36 -0500

    express app init with env support. generate env types. eslint recommended config

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello world");
+import "dotenv/config";
+import express from "express";
+import os from "os";
+
+const main = async () => {
+  // initializing the express app
+  const app = express();
+
+  app.listen(process.env.PORT, () => {
+    console.log(
+      `@awesome-xyz/api is running ${os.hostname} at port ${process.env.PORT}`
+    );
+  });
+};
+
+main().catch((error) => {
+  console.error(error);
+});
--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,19 @@
-import typescriptParser from "@typescript-eslint/parser";
-import typescriptPlugin from "@typescript-eslint/eslint-plugin";
+import typescriptEslintParser from "@typescript-eslint/parser";
+import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin";
+import js from '@eslint/js';
 
 export default [{
+    ...js.configs.recommended,
+    // TODO: add recommend configs from typescript-eslint once they start officially supporting flat config
     files: ["api/**/*.ts"],
     languageOptions: {
         ecmaVersion: "latest",
         sourceType: "module",
-        parser: typescriptParser
+        parser: typescriptEslintParser
     },
     plugins: {
-        typescriptPlugin
+        typescriptPlugin: typescriptEslintPlugin
     },
     rules: {
-        "no-console": "error"
     }
 }]
